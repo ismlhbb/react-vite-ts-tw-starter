05204621f24fcd2bde035f38833d4eea5f5fb412
Ismail Habibi Herman <ismail.habibi2104@gmail.com>
2021-11-09 15:30:17 +0700

    refactor: revert back 2 commits

--- src/layouts/home/HomeLayout.tsx
+++ src/layouts/home/HomeLayout.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import NotFoundPage from 'pages/404';
 import PageLoadingBar from 'components/PageLoadingBar';
 import Header from 'components/header/Header';
 
-import NotFoundPage from 'pages/404';
-import HomePage from 'pages/home';
-import CounterPage from 'pages/counter';
-import ExamplePage from 'pages/example';
+const Home = React.lazy(() => import('pages/home'));
+const CounterPage = React.lazy(() => import('pages/counter'));
+const ExamplePage = React.lazy(() => import('pages/example'));
 
 const HomeLayout = () => {
   return (
@@ -18,7 +18,7 @@ const HomeLayout = () => {
             <PageLoadingBar isFallback={false} />
             <React.Suspense fallback={<PageLoadingBar isFallback={true} />}>
               <Switch>
-                <Route exact path='/' component={HomePage} />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/counter' component={CounterPage} />
                 <Route exact path='/example' component={ExamplePage} />
                 <Route path='*' component={NotFoundPage} />
